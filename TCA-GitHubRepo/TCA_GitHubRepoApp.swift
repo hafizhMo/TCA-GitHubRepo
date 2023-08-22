0350d378145e5386d6d98f912b7f26f1c4fe5251
hafizhMo <hafizh.hizb@gmail.com>
2023-08-22 22:09:40 +0700

    Initial Commit

---
+++ TCA-GitHubRepo/TCA_GitHubRepoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_GitHubRepoApp.swift
+//  TCA-GitHubRepo
+//
+//  Created by Hafizh Mo on 22/08/23.
+//
+
+import SwiftUI
+
+@main
+struct TCA_GitHubRepoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
